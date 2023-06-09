561deb582a5d6ebf4948e1f316f5d49acdd6e5b1
basiacarvalho <basiacarvalho@gmail.com>
2023-06-09 15:24:10 +0200

    movie poster placeholder

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const config = {
   },
 };
 
+renderMoviePlaceholders();
 loadPopularMovies();
 
 async function loadPopularMovies() {
@@ -92,4 +93,14 @@ function getGenresNames(genres, genre_ids) {
     filteredGenres = genresList;
   }
   return filteredGenres.join(', ');
+}
+
+function renderMoviePlaceholders() {
+  const placeholders = [];
+  for (let i = 0; i < 20; i++) {
+    placeholders.push(`<div class="movie-card--placeholder">
+      <h3 class="movie-card--placeholder__title">Movie Poster Placeholder</h3>
+    </div>`);
+  }
+  movieListContainer.innerHTML = placeholders.join(' ');
 }
