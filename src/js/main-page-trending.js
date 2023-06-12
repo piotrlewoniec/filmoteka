b1dce3502389be7605c7ddac0a8a12d4a881e54b
piotrlewoniec <117780551+piotrlewoniec@users.noreply.github.com>
2023-06-12 15:08:49 +0200

    added paggination for trending

--- src/js/main-page-trending.js
+++ src/js/main-page-trending.js
@@ -7,18 +7,39 @@ import {
 } from './config/stdquery';
 import { apikeyTMDB } from './config/apikey';
 import { renderMovieList } from './ui/cardgen';
+import { setPagination } from './ui/pagination';
 
 const movieListContainer = document.querySelector('.movie-list-container');
+const paginationContainer = document.querySelector('.pagination');
 
 renderMoviePlaceholders(movieListContainer);
 
 const trendingMoviesGet = async () => {
   const header = { ...defaultHeaderGet, ...trendingMovieTOP20Url };
-  const parameters = { ...trendingMovieTOP20Params, api_key: apikeyTMDB };
+  const parameters = { ...trendingMovieTOP20Params, api_key: apikeyTMDB, page: 1 };
   const movies = await axiosGetData(header, parameters);
-  return movies;
+  const data = {
+    header: header,
+    parameters: parameters,
+    movieListContainer: movieListContainer,
+    paginationContainer: paginationContainer,
+    movies: movies,
+  };
+  return data;
 };
 
 trendingMoviesGet()
-  .then(movies => renderMovieList(movieListContainer, movies.data.results))
+  .then(data => displayResult(data))
   .catch(error => console.log(error));
+
+function displayResult(data) {
+  renderMovieList(movieListContainer, data.movies.data.results);
+  setPagination({
+    headerRef: data.header,
+    parametersRef: data.parameters,
+    movieListContainer: data.movieListContainer,
+    paginationContainerRef: data.paginationContainer,
+    currentPageRef: data.movies.data.page,
+    totalPagesRef: data.movies.data.total_pages,
+  });
+}
