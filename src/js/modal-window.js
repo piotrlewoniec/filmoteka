7387128090ea8b22508aae39c573a5202bf53bf0
Jakub Pawlik <jakub.pawlik@vp.pl>
2023-06-13 13:53:10 +0200

    Added modal window for movie infod

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -2,17 +2,44 @@ const movieListContainer = document.querySelector('.movie-list-container');
 
 movieListContainer.addEventListener('click', showMovieModal);
 
-function showMovieModal() {
+function showMovieModal(event) {
+  if (event.target.type === 'button') {
+    return;
+  }
   const backdrop = document.querySelector('.movie-backdrop');
   const movieWindowContent = document.querySelector('.movie-modal');
   backdrop.classList.remove('is-hidden');
   movieWindowContent.insertAdjacentHTML(
     'beforeend',
     `<div>
-<p>informacje o filmie</p> </div>`,
+<p>informacje o filmie - DELEGACJA</p> </div>`,
   );
   backdrop.onclick = function () {
     backdrop.classList.add('is-hidden');
     movieWindowContent.innerHTML = '';
   };
 }
+
+// const movieCards = document.querySelectorAll('.movie-card');
+// for (const movieCard of movieCards) {
+//   movieCard.addEventListener('click', event => showMovieModal(event));
+// }
+
+// function showMovieModal(event) {
+//   if (event.target.type == 'button') {
+//     return;
+//   }
+//   const backdrop = document.querySelector('.movie-backdrop');
+//   const movieWindowContent = document.querySelector('.movie-modal');
+//   backdrop.classList.remove('is-hidden');
+//   movieWindowContent.insertAdjacentHTML(
+//     'beforeend',
+//     `<div>
+
+// <p>informacje o filmie - BEZ DELEGACJI</p> </div>`,
+//   );
+//   backdrop.onclick = function () {
+//     backdrop.classList.add('is-hidden');
+//     movieWindowContent.innerHTML = '';
+//   };
+// }
