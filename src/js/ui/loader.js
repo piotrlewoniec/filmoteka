330a3e922e9438bdde380783adc482c85c35556a
RadekKisiela <radek1997_97@o2.pl>
2023-06-17 14:45:53 +0200

    update loader

--- src/js/ui/loader.js
+++ src/js/ui/loader.js
@@ -1,7 +1,45 @@
 function showHideLoader(element) {
-    if (element.style.display === 'block') {
-      element.style.display = 'none';
-    } else {
-      element.style.display = 'block';
-    }
+  if (element.style.display === 'block') {
+    element.style.display = 'none';
+  } else {
+    element.style.display = 'block';
   }
+}
+
+function loadContent() {
+  const loaderElement = document.getElementById('loader');
+  showHideLoader(loaderElement); 
+
+  
+  fetchData()
+    .then(data => {
+      
+      showHideLoader(loaderElement);
+
+      renderData(data);
+    })
+    .catch(error => {
+      
+      showHideLoader(loaderElement);
+
+      showError(error);
+    });
+}
+
+function fetchData() {
+  const apiUrl = `https://api.themoviedb.org/3/movie/12345?api_key=${apikeyTMDB}`;
+
+  return fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      return data;
+    });
+}
+
+function renderData(data) {
+  console.log('Tytuł filmu:', data.title);
+}
+
+function showError(error) {
+  console.error('Błąd ładowania danych:', error);
+}
