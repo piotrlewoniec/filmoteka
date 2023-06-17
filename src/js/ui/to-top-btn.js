b7e200aba9591fa8f691e12952ed6231916bfa0f
Dawid Wrobel <118534055+Dawidos12352@users.noreply.github.com>
2023-06-17 15:26:49 +0200

    added to-top-btn function in js

--- src/js/ui/to-top-btn.js
+++ src/js/ui/to-top-btn.js
@@ -1,25 +1,12 @@
 window.addEventListener('scroll', function() {
-  const button = document.querySelector('.scroll-to-top-button');
-  const scrollPosition = window.scrollY;
-
- 
-  if (scrollPosition > window.innerHeight) {
-    button.classList.add('show');
-  } else {
-    button.classList.remove('show');
-  }
-});
-
-document.querySelector('.scroll-to-top-button').addEventListener('click', function() {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
+    const scrollButton = document.querySelector('.scroll-to-top-button');
+    const screenHeight = window.innerHeight;
+  
+    if (window.scrollY > screenHeight) {
+      scrollButton.style.display = 'block';
+    } else {
+      scrollButton.style.display = 'none';
+    }
   });
-});
-
-
-
-
-
 
 
